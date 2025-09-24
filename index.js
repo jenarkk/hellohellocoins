bb28505f28704689e775997fbd91702d6419d688
jenarkk <jenark1010yt@gmail.com>
2025-09-24 23:20:16 +0100

    caching, message improvements

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import hhcCommand from "./events/hhcCommand";
 import config from "./config";
 
 import "./features/craft";
+import "./features/lowball";
 
 hhcCommand.addCommand(undefined, "Opens the config gui", () => {
     config.openGUI();
